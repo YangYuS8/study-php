a49155c38a49a57d7299dfc123dba86e729fecf0
YangYuS8 <YangYuS8@163.com>
2026-05-25 17:50:41 +0800

    fix: 修复文件夹命名错误

--- src/第五章练习题/字符串2.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getExt($filename) {
-    return pathinfo($filename, PATHINFO_EXTENSION);
-}
-
-$file = "document.docx";
-echo "文件名：" . $file . "<br>";
-echo "后缀名：" . getExt($file);
-?>
